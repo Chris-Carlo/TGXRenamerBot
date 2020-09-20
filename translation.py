38f4721729c27f775c9df2b77fdef21aedc4318e
DEVA NANDANAN B H <63557312+Chris-Carlo@users.noreply.github.com>
2020-09-20 12:35:58 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,13 +1,13 @@
 class Translation(object):
-    START_TEXT = """Hello Dear! <b>{}</b>,
+    START_TEXT = """Hai<b>{}</b>,
 
-<code>I'm A Simple File Renamer bot and can upload as File or Video With Permanent Thumbnail support!💯.</code> \n\n<b>Bot Maintained By:</b> <a href='https://t.me/MaxWizard'>Maxx Wizard 👨‍🎤</a>
+<code>I'm a Simple File Renamer bot and can upload as File or Video With Permanent Thumbnail support!💯.</code> \n\n<b>Bot Created By:</b> <a href='https://t.me/Chris_Carlo'>Chris Carlo 👨‍🎤</a>
 
 Press help button which is available at the bottom to know how to use me."""
 
-    START_TEXT_1 = """Hello Dear,
+    START_TEXT_1 = """Hai,
 
-<code> I'm A Simple File Renamer bot and can upload as File or Video With Permanent Thumbnail support!💯.</code> \n\n<b>Bot Maintained By:</b> <a href='https://t.me/MaxWizard'>Maxx Wizard 👨‍🎤</a>
+<code> I'm a Simple File Renamer bot and can upload as File or Video With Permanent Thumbnail support!💯.</code> \n\n<b>Bot Maintained By:</b> <a href='https://t.me/Darklester'>Darklester 👨‍🎤</a>
 
 📍For more info /help
 
@@ -20,21 +20,21 @@ Press help button which is available at the bottom to know how to use me."""
     DOWNLOAD_START = "<tt>Downloading to my server....⬇️</tt>"
     UPLOAD_START_VIDEO = "<tt>Uploading as video...⬆️</tt>"
     UPLOAD_START = "<tt>Uploading as File...⬆️</tt>"
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations.I can't do anything for that 🤷‍♂️."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using [Maxx Bots](https://t.me/MaxxBots)'s bot.**"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations.I can't do anything for that 😢."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using [TECHNOGRAM X](https://t.me/TGXTECH)'s bot.**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
-    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail saved✅️. This image will be used in the File."
+    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail SAVED ✅️. This image will be used in the File."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
-    CUSTOM_CAPTION_UL_FILE = "<b>{}</b> \n\n@MaxxBots"
+    CUSTOM_CAPTION_UL_FILE = "<b>{}</b> \n\n@TGXTECH"
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     HELP_USER = """<b>Hey! Here you can know how to use me</b>, 
     
-<tt>You Need help ?? 😅</tt>
+<tt>You Need help ?</tt>
     
 1.◽<b>Send me any Telegram File or Video.</b>
 
@@ -44,13 +44,13 @@ Press help button which is available at the bottom to know how to use me."""
 
 4.◽<b>Send A Photo to make it as permanent Thumbnail</b>   
  
-<b>❤️Made With Love:======= </b> @MaxWizard & @Ns_AnoNymous.
+<b>❤️ Made With Love:======= </b> @Chris_Carlo & @Darklester.
 
-<b>Thanks to My Boss :</b><a href="https://t.me/Ns_AnoNymous">Owner 👨‍🏫</a> and <a href='https://t.me/MaxWizard'>MaxWizard👨‍🎤</a>
+<b>Thanks to My Boss :</b><a href="https://t.me/Darklester">Owner 👨‍🏫</a> and <a href='https://t.me/Chris_Carlo'>Chris Carlo 👨‍🎤</a>
 
 
-🗣𝐃𝐢𝐬𝐜𝐮𝐬𝐬 𝐆𝐫𝐨𝐮𝐩: @MaxxBotChat
-📢𝐔𝐩𝐝𝐚𝐭𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥: @MaxxBots"""
+🗣𝐃𝐢𝐬𝐜𝐮𝐬𝐬 𝐆𝐫𝐨𝐮𝐩: @TGXTECHGroup
+📢𝐔𝐩𝐝𝐚𝐭𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥: @TGXTECH"""
 
   
     REPLY_TO_DOC_FOR_RENAME_FILE = "🤦‍♂️ Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.\n\n(For uploading as file).\n\nSee /help for more information. "
@@ -62,48 +62,48 @@ Free users only 1 request per 30 minutes.
     IFLONG_FILE_NAME = """File Name limit allowed by Telegram is {alimit} characters.
 The given file name has {num} characters.
 
-<b>Essays Not allowed in Telegram file name!</b>
-©️ <code>@MaxxBots</code>
+<b>Short your file name please.</b>
+©️ <code>@TGXTECH</code>
 Please short your file name and try again!"""
     ABOUT = """Hi {},
 
-🤖 My Name: [Max Rename Bot](https://t.me/MaxxRename_Bot)
+🤖 My Name: [TGX Renamer Bot](t.me/RenamerTGXBOT)
     
 📝 Language: [Python 3](https://www.python.org/)
 
 📫 Framework: [Pyrogram](https://github.com/pyrogram/pyrogram)
 
-👨‍💻 Developer: [MaxxRider](https://t.me/MaxxRider)
+👨‍💻 Developer: [Chris Carlo](https://t.me/Chris_Carlo)
 
-📢 Channel: [MAX BOTS UPDATE](https://t.me/MaxxBots)
+📢 Channel: [TECHNOGRAM X](https://t.me/TGXTECH)
 
-👥 Group: [SUPPORT GROUP](https://t.me/MaxxBotChat)
+👥 Group: [TGX SUPPORT GROUP](https://t.me/TGXTECHGroup)
 
 ⚖️ Credit: `Everyone in this journey`
 
 🌎 Server: [HEROKU](https://heroku.com)
 
 
-🛡️ Powered By: [MOVIE DOWNLOAD HUB](https://t.me/joinchat/AAAAAFfWz-izyEXi-mpiQQ)"""
+🛡️ Powered By: [DARKLESTER](https://t.me/Darklester)"""
 
 
 
     ABOUT_1 = """**MY DETAILS :**
-🤖 My Name: [Max Rename Bot](https://t.me/MaxxRename_Bot)
+🤖 My Name: [TGX Renamer Bot](t.me/RenamerTGXBOT)
     
 📝 Language: [Python 3](https://www.python.org/)
 
 📫 Framework: [Pyrogram](https://github.com/pyrogram/pyrogram)
 
-👨‍💻 Developer: [MaxxRider](https://t.me/MaxxRider)
+👨‍💻 Developer: [Chris Carlo](https://t.me/Chris_Carlo)
 
-📢 Channel: [MAX BOTS UPDATE](https://t.me/MaxxBots)
+📢 Channel: [TECHNOGRAM X](https://t.me/TGXTECH)
 
-👥 Group: [SUPPORT GROUP](https://t.me/MaxxBotChat)
+👥 Group: [TGX SUPPORT GROUP](https://t.me/TGXTECHGroup)
 
 ⚖️ Credit: `Everyone in this journey`
 
 🌎 Server: [HEROKU](https://heroku.com)
 
 
-🛡️ Powered By: [MOVIE DOWNLOAD HUB](https://t.me/joinchat/AAAAAFfWz-izyEXi-mpiQQ)"""
+🛡️ Powered By: [DARKLESTER](https://t.me/Darklester)"""
